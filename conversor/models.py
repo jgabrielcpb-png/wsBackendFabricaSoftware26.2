b0a738e6697f66cd9afebfddee9e7aba7194e02f
jgabrielcpb-png <j.gabrielcpb@gmail.com>
2026-08-29 01:41:08 -0300

    feat: criaçao  models Moeda e Conversao com relacionamento por FK

--- conversor/models.py
+++ conversor/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
+class Moeda(models.Model):
+    codigo = models.CharField(max_length=10, unique=True)  
+    nome = models.CharField(max_length=100)  
+
+    def __str__(self):
+        return self.codigo
+
+
+class Conversao(models.Model):
+    moeda_origem = models.ForeignKey(Moeda, related_name='conversoes_origem', on_delete=models.CASCADE)
+    moeda_destino = models.ForeignKey(Moeda, related_name='conversoes_destino', on_delete=models.CASCADE)
+    valor_original = models.DecimalField(max_digits=15, decimal_places=2)
+    valor_convertido = models.DecimalField(max_digits=15, decimal_places=2)
+    data_conversao = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'{self.valor_original} {self.moeda_origem.codigo} -> {self.valor_convertido} {self.moeda_destino.codigo}'
+
 # Create your models here.
